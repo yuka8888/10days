0ea170de7ed55550f812c316deb7702ae1eff253
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-09-10 11:21:02 +0900

    男の子リソース追加　箱押すモーション未完成

--- PlayerBottom.cpp
+++ PlayerBottom.cpp
@@ -33,6 +33,61 @@ void PlayerBottom::Draw(Camera camera)
 
 	Novice::DrawBox(int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f), (int)kWidth_, (int)kHeight_, 0.0f, BLUE, kFillModeSolid);
 
+	switch (direction)
+	{
+	case kRight:
+
+		Novice::DrawQuad(int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			(int)kWidth_ * (animationTimer / 7), 0, (int)kWidth_, (int)kHeight_, walkRight,
+			WHITE);
+
+		break;
+	case kRightStand:
+		Novice::DrawQuad(int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			(int)kWidth_ * (animationTimer / 30), 0, (int)kWidth_, (int)kHeight_, standingRight,
+			WHITE);
+		break;
+	case kLeft:
+
+		Novice::DrawQuad(int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			(int)kWidth_ * (animationTimer / 7), 0, (int)kWidth_, (int)kHeight_, walkLeft,
+			WHITE);
+
+		break;
+	case kLeftStand:
+		Novice::DrawQuad(int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x - kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			int(screenPosition_.x + kWidth_ / 2.0f), int(screenPosition_.y + kHeight_ / 2.0f),
+			(int)kWidth_ * (animationTimer / 30), 0, (int)kWidth_, (int)kHeight_, standingLeft,
+			WHITE);
+		break;
+	case kPushBlock:
+		Novice::DrawQuad(int(screenPosition_.x - pushWidth / 2.0f) - 7, int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x + pushWidth / 2.0f) - 7, int(screenPosition_.y - kHeight_ / 2.0f),
+			int(screenPosition_.x - pushWidth / 2.0f) - 7, int(screenPosition_.y + kHeight_ / 2.0f),
+			int(screenPosition_.x + pushWidth / 2.0f) - 7, int(screenPosition_.y + kHeight_ / 2.0f),
+			(int)pushWidth * (animationTimer / 7), 0, (int)pushWidth, (int)kHeight_, pushBlock,
+			WHITE);
+		break;
+
+	case kRightJump:
+		break;
+	case kLeftJump:
+		break;
+	default:
+		break;
+	}
+
 #ifdef _DEBUG
 	ImGui::DragFloat2("PlayerBoy.Translation", &translation_.x, 0.01f);
 	ImGui::DragFloat2("PlayerBoy.Velocity", &velocity_.x, 0.01f);
@@ -43,9 +98,18 @@ void PlayerBottom::Draw(Camera camera)
 void PlayerBottom::Move()
 {
 	isPushTwoBlocks_ = false;
+	isPushBlock_ = false;
 
+	animationTimer++;
+	AnimationTimerChange();
 	velocity_ = { 0.0f, 0.0f };
-
+	//directionを待機に切り替え
+	if (direction == Direction::kLeft) {
+		direction = Direction::kLeftStand;
+	}
+	else if (direction == Direction::kRight) {
+		direction = Direction::kRightStand;
+	}
 	//移動処理
 	if (Novice::CheckHitKey(DIK_A) || Novice::CheckHitKey(DIK_LEFTARROW)) {
 		direction = Direction::kLeft;
@@ -55,7 +119,23 @@ void PlayerBottom::Move()
 		direction = Direction::kRight;
 		velocity_.x = 2.0f;
 	}
-	 
+	////止まったらdirectionを待機に切り替え
+	if (velocity_.x == 0) {
+		if (direction == Direction::kRight) {
+
+			direction = Direction::kRightStand;
+		}
+		else if (direction == Direction::kLeft) {
+			direction = Direction::kLeftStand;
+		}
+	}
+	////ブロックが押されていたらdirectionをpushに切り替え
+	//if (isPushBlock_) {
+	//	direction = Direction::kPushBlock;
+	//}
+	//if (isPushBlock_ == false) {
+	//	direction = Direction::kRightStand;
+	//}
 	//進んだ先のaabbの更新
 	aabb_.max = { translation_.x + velocity_.x + kWidth_ / 2, translation_.y + velocity_.y + kHeight_ / 2 };
 	aabb_.min = { translation_.x + velocity_.x - kWidth_ / 2, translation_.y + velocity_.y - kHeight_ / 2 };
@@ -89,7 +169,7 @@ AABB PlayerBottom::GetAABB()
 void PlayerBottom::OnCollision()
 {
 	isPushBlock_ = true;
-
+	
 	//移動量を減らす
 	velocity_.x = velocity_.x * 0.30f;
 
@@ -127,4 +207,38 @@ Vector2 PlayerBottom::GetSize()
 void PlayerBottom::SetTranslation(Vector2 translation)
 {
 	translation_ = translation;
+}
+
+void PlayerBottom::AnimationTimerChange()
+{
+
+	switch (direction)
+	{
+	case kRight:
+		animationTimerReset = 56;
+		break;
+	case kRightStand:
+		animationTimerReset = 60;
+		break;
+	case kLeft:
+		animationTimerReset = 56;
+		break;
+	case kLeftStand:
+		animationTimerReset = 60;
+		break;
+	case kPushBlock:
+		animationTimerReset = 63;
+		break;
+	case kRightJump:
+		break;
+	case kLeftJump:
+		break;
+
+	default:
+		break;
+	}
+
+	if (animationTimer >= animationTimerReset) {
+		animationTimer = 0;
+	}
 }
